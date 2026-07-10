86ac89b2d813b1e7329b94a1e494ce7836c052d9
fekaram <contato@fksa.com.br>
2026-07-10 16:59:18 -0300

    Add base path for Health-Weight-Pro
    Set base path for deployment in Vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/Health-Weight-Pro/',
+
   appType: 'spa',
+
   build: {
     outDir: 'dist',
     sourcemap: true,
   },
+
   server: {
     host: '127.0.0.1',
     port: 5173,
